f426adf1f1f6de360922ec2af7c766ab7e9abe3d
Akin Campbell <54187807+akincampbell@users.noreply.github.com>
2023-10-13 19:09:29 +0000

    Added own photos to users#show and commenting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "photos#index"
-  get "/:username" => "users#show", as: :user
+  
 
   devise_for :users
 
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :comments
   resources :photos
-  resources :users, only: :show
+
+  # Must be at end
+  get "/:username" => "users#show", as: :user
 end
